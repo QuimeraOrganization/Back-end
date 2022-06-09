5dee83ccac2fb20c7e2c31bcedf07707c25cea83
Rebeca Moreira <becamoreira22@gmail.com>
2022-06-09 20:25:31 -0300

    refactor: changes accepted

--- src/routes.js
+++ src/routes.js
@@ -1,17 +1,9 @@
-<<<<<<< HEAD
-import { Router } from "express";
-import { UsersController } from "./controllers/UserController.js";
-import { TokenController } from "./controllers/tokenController/TokenController.js";
-import { FeedbackController } from "./controllers/FeedbackController.js";
-import { ProductController } from "./controllers/ProductController.js";
-import loginRequired from "./middlewars/loginRequired.js";
-=======
 import { Router } from 'express';
 import { UsersController } from './controllers/UserController.js';
 import { TokenController } from './controllers/tokenController/TokenController.js';
 import { FeedbackController } from './controllers/FeedbackController.js';
+import { ProductController } from './controllers/ProductController.js';
 import loginRequired from './middlewars/loginRequired.js';
->>>>>>> refactor: moved routes to follow patterns
 
 const router = Router();
 const token = new TokenController();
@@ -22,53 +14,31 @@ const productController = new ProductController();
 
 //USER-ROUTES
 //POSTS
-<<<<<<< HEAD
-router.post("/users", usersController.createUser);
-router.post("/feedbacks", feedbackController.createFeedback);
-router.post("/products", productController.save);
-
-//GETS
-router.get("/users/:id", loginRequired, usersController.findUser);
-router.get("/users", usersController.findAllUsers);
-router.get("/feedbacks/:id", loginRequired, feedbackController.findFeedback);
-router.get("/feedbacks", feedbackController.findAllFeedbacks);
-router.get("/products/:id", productController.findById);
-router.get("/products", productController.findAll);
-
-//UPDATES
-router.put("/users/:id", loginRequired, usersController.updateUser);
-router.put("/feedbacks/:id", loginRequired, feedbackController.updateFeedback);
-router.put("/products/:id", loginRequired, productController.update);
-
-//DELETES
-router.delete("/users/:id", loginRequired, usersController.deleteUser);
-router.delete("/feedbacks/:id", loginRequired, feedbackController.deleteFeedback);
-router.delete("/products/:id", loginRequired, productController.delete);
-=======
 router.post('/users', usersController.createUser);
+router.post('/feedbacks', feedbackController.createFeedback);
+router.post('/products', productController.save);
+
 //GETS
-router.get('/users', usersController.findAllUsers);
 router.get('/users/:id', loginRequired, usersController.findUser);
+router.get('/users', usersController.findAllUsers);
+router.get('/feedbacks/:id', loginRequired, feedbackController.findFeedback);
+router.get('/feedbacks', feedbackController.findAllFeedbacks);
+router.get('/products/:id', productController.findById);
+router.get('/products', productController.findAll);
+
 //UPDATES
 router.put('/users/:id', loginRequired, usersController.updateUser);
+router.put('/feedbacks/:id', loginRequired, feedbackController.updateFeedback);
+router.put('/products/:id', loginRequired, productController.update);
+
 //DELETES
 router.delete('/users/:id', loginRequired, usersController.deleteUser);
-
-//FEEDBACK-ROUTES
-//POSTS
-router.post('/feedbacks', feedbackController.createFeedback);
-//GETS
-router.get('/feedbacks', feedbackController.findAllFeedbacks);
-router.get('/feedbacks/:id', loginRequired, feedbackController.findFeedback);
-//UPDATES
-router.put('/feedbacks/:id', loginRequired, feedbackController.updateFeedback);
-//DELETES
 router.delete(
   '/feedbacks/:id',
   loginRequired,
   feedbackController.deleteFeedback,
 );
->>>>>>> refactor: moved routes to follow patterns
+router.delete('/products/:id', loginRequired, productController.delete);
 
 //TOKEN ROUTES
 router.post('/token', token.handle);
