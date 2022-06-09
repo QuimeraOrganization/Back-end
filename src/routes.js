6299d8982c4f768f7ea551cafc1174137a809447
Rebeca Moreira <becamoreira22@gmail.com>
2022-06-08 21:53:10 -0300

    refactor: moved routes to follow patterns

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,17 @@
+<<<<<<< HEAD
 import { Router } from "express";
 import { UsersController } from "./controllers/UserController.js";
 import { TokenController } from "./controllers/tokenController/TokenController.js";
 import { FeedbackController } from "./controllers/FeedbackController.js";
 import { ProductController } from "./controllers/ProductController.js";
 import loginRequired from "./middlewars/loginRequired.js";
+=======
+import { Router } from 'express';
+import { UsersController } from './controllers/UserController.js';
+import { TokenController } from './controllers/tokenController/TokenController.js';
+import { FeedbackController } from './controllers/FeedbackController.js';
+import loginRequired from './middlewars/loginRequired.js';
+>>>>>>> refactor: moved routes to follow patterns
 
 const router = Router();
 const token = new TokenController();
@@ -12,7 +20,9 @@ const usersController = new UsersController();
 const feedbackController = new FeedbackController();
 const productController = new ProductController();
 
+//USER-ROUTES
 //POSTS
+<<<<<<< HEAD
 router.post("/users", usersController.createUser);
 router.post("/feedbacks", feedbackController.createFeedback);
 router.post("/products", productController.save);
@@ -34,9 +44,34 @@ router.put("/products/:id", loginRequired, productController.update);
 router.delete("/users/:id", loginRequired, usersController.deleteUser);
 router.delete("/feedbacks/:id", loginRequired, feedbackController.deleteFeedback);
 router.delete("/products/:id", loginRequired, productController.delete);
+=======
+router.post('/users', usersController.createUser);
+//GETS
+router.get('/users', usersController.findAllUsers);
+router.get('/users/:id', loginRequired, usersController.findUser);
+//UPDATES
+router.put('/users/:id', loginRequired, usersController.updateUser);
+//DELETES
+router.delete('/users/:id', loginRequired, usersController.deleteUser);
+
+//FEEDBACK-ROUTES
+//POSTS
+router.post('/feedbacks', feedbackController.createFeedback);
+//GETS
+router.get('/feedbacks', feedbackController.findAllFeedbacks);
+router.get('/feedbacks/:id', loginRequired, feedbackController.findFeedback);
+//UPDATES
+router.put('/feedbacks/:id', loginRequired, feedbackController.updateFeedback);
+//DELETES
+router.delete(
+  '/feedbacks/:id',
+  loginRequired,
+  feedbackController.deleteFeedback,
+);
+>>>>>>> refactor: moved routes to follow patterns
 
 //TOKEN ROUTES
-router.post("/token", token.handle);
+router.post('/token', token.handle);
 
 /*
 rota para lougout, destruir o token.
