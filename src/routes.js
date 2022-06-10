c4d3d3635130320dd720d65366d4ccb559f6a8c5
Eros Linnyker <linnykereros@gmail.com>
2022-06-10 14:58:51 -0300

    Atualização na organização dos arquivos routes

--- src/routes.js
+++ src/routes.js
@@ -1,47 +1,18 @@
-import { Router } from 'express';
-import { UsersController } from './controllers/UserController.js';
-import { TokenController } from './controllers/tokenController/TokenController.js';
-import { FeedbackController } from './controllers/FeedbackController.js';
-import { ProductController } from './controllers/ProductController.js';
-import loginRequired from './middlewars/loginRequired.js';
+import { Router } from "express";
+import { TokenController } from "./controllers/tokenController/TokenController.js";
+import { userRoutes } from "./routes/userRoutes.js";
+import { productRoutes } from "./routes/productRoutes.js";
+import { feedbackRoutes } from "./routes/feedbackRoutes.js";
 
 const router = Router();
 const token = new TokenController();
 
-const usersController = new UsersController();
-const feedbackController = new FeedbackController();
-const productController = new ProductController();
-
-//USER-ROUTES
-//POSTS
-router.post('/users', usersController.createUser);
-router.post('/feedbacks', feedbackController.createFeedback);
-router.post('/products', productController.save);
-
-//GETS
-router.get('/users/:id', loginRequired, usersController.findUser);
-router.get('/users', usersController.findAllUsers);
-router.get('/feedbacks/:id', loginRequired, feedbackController.findFeedback);
-router.get('/feedbacks', feedbackController.findAllFeedbacks);
-router.get('/products/:id', productController.findById);
-router.get('/products', productController.findAll);
-
-//UPDATES
-router.put('/users/:id', loginRequired, usersController.updateUser);
-router.put('/feedbacks/:id', loginRequired, feedbackController.updateFeedback);
-router.put('/products/:id', loginRequired, productController.update);
-
-//DELETES
-router.delete('/users/:id', loginRequired, usersController.deleteUser);
-router.delete(
-  '/feedbacks/:id',
-  loginRequired,
-  feedbackController.deleteFeedback,
-);
-router.delete('/products/:id', loginRequired, productController.delete);
+router.use("/users", userRoutes);
+router.use("/products", productRoutes);
+router.use("/feedbacks", feedbackRoutes);
 
 //TOKEN ROUTES
-router.post('/token', token.handle);
+router.post("/token", token.handle);
 
 /*
 rota para lougout, destruir o token.
@@ -49,15 +20,7 @@ app.post('/logout', function(req, res) {
     res.json({ authorization: false, token: null });
 })
 
-//POST
-const createUser = new UsersController();
-//GET
-const findUser = new UsersController();
-const findAllUsers = new UsersController();
-//UPDATE
-const updateUser = new UsersController();
-//DELETE
-const deleteUser = new UsersController();
+
 
 */
 export { router };
