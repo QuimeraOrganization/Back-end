cc1b4eea4351d2f83371e54a9c51010710cfe3d0
Eros Linnyker <linnykereros@gmail.com>
2022-06-10 15:08:39 -0300

    Correção de import

--- src/serve.js
+++ src/serve.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { router } from "./routes.js";
+import { router } from "./routes/router.js";
 const app = express();
 const port = 3333;
 app.use(express.json());
