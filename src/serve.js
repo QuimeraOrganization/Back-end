a18e30ce2e54b36105c3a306f787521edfa68bf6
Edmundo Vitor <edmundovitor1414@hotmail.com>
2022-06-17 20:37:38 -0300

    Revert "Feature/crud-ingredients"

--- src/serve.js
+++ src/serve.js
@@ -1,13 +1,8 @@
-import "express-async-errors"
 import express from "express";
-import { ExceptionHandler } from "./middlewars/ExceptionHandler.js";
-import { router } from "./routes.js";
-
+import { router } from "./routes/router.js";
 const app = express();
 const port = 3333;
-
 app.use(express.json());
 app.use(router);
-app.use(ExceptionHandler)
 
 app.listen(port, () => console.log(`Running http://localhost:${port}`));
--- src/validators/ingredients/putIngredientsValidator.js
+++
@@ -1,5 +0,0 @@
-import * as yup from "yup";
-
-export const putIngredientsValidator = yup.object({
-  name: yup.string().required()
-});
