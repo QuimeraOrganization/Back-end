d17eb152a96f8971e0959c8ed7459de37e001970
Eros Linnyker <linnykereros@gmail.com>
2022-06-25 11:03:08 -0300

    Swagger autogen

--- src/routes/productRoutes.js
+++ src/routes/productRoutes.js
@@ -9,36 +9,241 @@ import { putProductValidator } from "../validators/products/putProductValidator.
 import multer from "multer";
 
 const Multer = multer({
-  storage: multer.memoryStorage()
+  storage: multer.memoryStorage(),
 });
 
-productRoutes.post("/", loginRequired, Multer.single("image"), validateRequest(postProductValidator), productsController.save
+productRoutes.post(
+  "/",
+  loginRequired,
+  Multer.single("image"),
+  validateRequest(postProductValidator),
+  productsController.save
   /* 
-    #swagger.tags = ['Products']
+   #swagger.summary = "Cadastro de Produto!"
+   #swagger.description = "Essa rota é responsável pela criação de Produtos!"
+   #swagger.tags = ['Products']
+
+   #swagger.requestBody = {
+      "content": {
+            "application/json": {
+              "schema": {
+                "type": "object",
+                "properties": {
+                  "name": {
+                    "type": "string"
+                  },
+                  "description": {
+                    "type": "string"
+                  },
+                  "image": {
+                    "type": "string"
+                  },
+                  "verified": {
+                    "type": "boolean"
+                  },
+                  "userId": {
+                    "type": "integer"
+                  }
+                },
+                "example": {
+                  "name": "macarrão",
+                  "description": "teste",
+                  "image": "teste",
+                  "verified": false,
+                  "userId": 1
+                }
+              }
+            }
+          }
+   }
+   #swagger.responses[201] = {
+      "description": "Produto cadastrado com sucesso!"
+   }
+   #swagger.responses[400] = {
+      "description": "Verifique se os campos necessários e as atribuições no exemplo estão corretas e tente novamente!"
+   }
+
   */
 );
 
-productRoutes.get("/", productsController.findAll
+productRoutes.get(
+  "/",
+  productsController.findAll
   /* 
-    #swagger.tags = ['Products']
+     #swagger.security = [{
+      "bearerAuth": []
+    }]
+    #swagger.summary = "Busca por todos os Produtos!"
+   #swagger.description = "Essa rota é responsável por buscar todos os Produtos cadastrados no banco!"
+   #swagger.tags = ['Products']
+
+  
+    #swagger.responses[200] = {
+      description: "Busca por todos os Produtos!",
+      content:{
+        "application/json": {
+          example: [
+            {
+            "name": "macarrão",
+                  "description": "gostoso",
+                  "image": "teste",
+                  "verified": false,
+                  "userId": 1
+            },
+            {
+             "name": "cuscuz",
+             "description": "flocão",
+             "image": "teste",
+             "verified": false,
+             "userId": 1
+            }
+        ]
+        }
+      }
+        
+    } 
+
   */
 );
 
-productRoutes.get("/:id", productsController.findById
+productRoutes.get(
+  "/:id",
+  productsController.findById
   /* 
-    #swagger.tags = ['Products']
+     #swagger.security = [{
+      "bearerAuth": []
+    }]
+    #swagger.summary = "Busca de Produtos pelo ID!"
+   #swagger.description = "Essa rota é responsável pela busca de Produto pelo ID!"
+   #swagger.tags = ['Products']
+
+  
+    #swagger.responses[200] = {
+      description: "Busca de Produtos pelo ID!",
+      content:{
+        "application/json": {
+          example: [
+            {
+            "name": "macarrão",
+                  "description": "gostoso",
+                  "image": "teste",
+                  "verified": false,
+                  "userId": 1
+            },
+            {
+             "name": "cuscuz",
+             "description": "flocão",
+             "image": "teste",
+             "verified": false,
+             "userId": 1
+            }
+        ]
+        }
+      }
+        
+    } 
+
+    #swagger.responses[404] = {
+       "description": "Produto não encontrado!"
+    }
+ 
+    #swagger.responses[400] = {
+       "description": "Solicitação Inválida"
+    }
+
   */
 );
 
-productRoutes.put("/:id", loginRequired, Multer.single("image"), validateRequest(putProductValidator), productsController.update
+productRoutes.put(
+  "/:id",
+  loginRequired,
+  Multer.single("image"),
+  validateRequest(putProductValidator),
+  productsController.update
   /* 
-    #swagger.tags = ['Products']
+  #swagger.security = [{
+      "bearerAuth": []
+    }]
+  #swagger.summary = "Atualização de Produto!"
+  #swagger.description = "Essa rota é responsável pela atualização do Produto!"
+  #swagger.tags = ['Products']
+  #swagger.parameters = [
+    {
+       "in": "path",
+       "name": "id",
+       "type": "string",
+       "required": true
+    }
+  ]
+
+  #swagger.requestBody = {
+    "content": {
+            "application/json": {
+              "schema": {
+                "type": "object",
+                "properties": {
+                  "name": {
+                    "type": "string"
+                  },
+                  "description": {
+                    "type": "string"
+                  },
+                  "image": {
+                    "type": "string"
+                  },
+                  "verified": {
+                    "type": "boolean"
+                  },
+                  "userId": {
+                    "type": "integer"
+                  }
+                },
+                "example": {
+                  "name": "macarrão",
+                  "description": "funcionou uhuu",
+                  "image": "teste",
+                  "verified": false,
+                  "userId": 1
+                }
+              }
+            }
+          }
+  }
+
+    #swagger.responses[200] = {
+       "description": "Produto atualizado com sucesso!"
+    }
+    #swagger.responses[400] = {
+       "description": "Solicitação Inválida!"
+    }
+
   */
 );
 
-productRoutes.delete("/:id", loginRequired, productsController.delete
+productRoutes.delete(
+  "/:id",
+  loginRequired,
+  productsController.delete
   /* 
-    #swagger.tags = ['Products']
+   #swagger.security = [{
+      "bearerAuth": []
+    }]
+  #swagger.summary = "Deleta um Produto!"
+  #swagger.description = "Essa rota é responsável por deletar um Produto!"
+  #swagger.tags = ['Products']
+  #swagger.parameters = [
+    {
+       "in": "path",
+       "name": "id",
+       "type": "string",
+       "required": true
+    }
+  ]
+
+  #swagger.responses[200] = {
+      "description": "Produto deletado com sucesso!"
+  }
+
   */
 );
 
--- src/routes/tokenRoutes.js
+++ src/routes/tokenRoutes.js
@@ -4,9 +4,43 @@ import { TokenController } from "../controllers/tokenController/TokenController.
 const tokenRoutes = Router();
 const tokenController = new TokenController();
 
-tokenRoutes.post("/", tokenController.handle
+tokenRoutes.post(
+  "/",
+  tokenController.handle
   /* 
+   #swagger.summary = "Criando token para autenticação do Usuário!"
+    #swagger.description = "Essa rota é responsável pela criação do Token para o Usuário conseguir se autenticar!"
     #swagger.tags = ['Authentication']
+    #swagger.requestBody = {
+      required: true,
+          "content": {
+            "application/json": {
+              "schema": {
+                "type": "object",
+                "properties": {
+                  "email": {
+                    "type": "string"
+                  },
+                  "password": {
+                    "type": "string"
+                  },
+                  "permission": {
+                    "type": "string"
+                  }
+                },
+                "example": {
+                  "email": "pedro_oliverira@gmail.com",
+                  "password": "13456"
+                }
+              }
+            }
+          }
+    }
+    #swagger.responses[201] = {
+      "description": "Token gerado com sucesso!"
+    }
+   
+
   */
 );
 
